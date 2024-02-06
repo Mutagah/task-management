1e7c432a8c757574a37f3a754b63f8a907662e05
Mutagah <kihahumutagah@gmail.com>
2024-02-07 00:32:19 +0300

    :seedling:feat: Addition of more dummy data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@ puts "Seeding of the database tables..."
 
         Task.create!(name:"CV review",description:"Revamping of resume/cv",status: status.sample)
 
+        Task.create!(name:"Do laundry", description:"One week laundry", status: status.sample)
+
+        Task.create!(name:"Wash dishes", description:"One day dishes",status: status.sample)
+
         Task.create!(name:"Test your api endpoints", description:"Test proper working of task endpoints and routes",status:status.sample)
 
 puts "End of seeding database tables!!!"

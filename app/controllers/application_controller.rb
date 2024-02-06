7165e6b104ecac4c92f4331b6bdb61226b67f21e
Mutagah <kihahumutagah@gmail.com>
2024-02-06 22:44:43 +0300

    :goal_net:feat: Catching of record not found error.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
     rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+    rescue_from ActiveRecord::RecordNotFound, with: :unfound_record
 
 
     def invalid_record invalid
         render json: {errors: invalid.record.errors.full_messages},status: :unprocessable_entity
     end
     
+    def unfound_record (error)
+        render json:{errors: error},status: :not_found
+    end
+
 end

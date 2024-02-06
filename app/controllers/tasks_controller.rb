3953dd83162f3cbabf32a33b2beb4c57f2f78704
Mutagah <kihahumutagah@gmail.com>
2024-02-06 22:42:43 +0300

    :bricks:construction: feat: Addition of task controller methods.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,33 @@
 class TasksController < ApplicationController
 
+    # Creating a task
+    def create
+        task = Task.create!(task_params)
+        render json: task, status: :created
+    end
 
     # Viewing all the tasks
     def index
         render json: Task.all, status: :ok
     end
 
-    # Method to create a task
-    def create
-        task = Task.create!(task_params)
-        render json: task, status: :created
+    # viewing a single task
+    def show
+        render json: find_task, status: :ok
     end
 
-    # Metbod for viewing a single task
+    # updating the status of a task
+    def update
+        task = find_task
+        task.update!(update_task_params)
+        render json: task,status: :accepted
+    end
+    
+    # getting the tasks via status of the task
+
+    # deleting all the tasks
+
+    # deleting a task by an Id
 
     private
     
@@ -20,5 +35,15 @@ class TasksController < ApplicationController
         params[:status]&.capitalize!
         params.permit(:name, :description, :status)
     end
+    
+    def find_task
+        Task.find(params[:id])
+    end
+
+    def update_task_params
+        params[:status]&.capitalize!
+        params.permit(:status)
+    end
+
 
 end
